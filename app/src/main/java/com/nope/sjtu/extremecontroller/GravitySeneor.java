ef823f0c2dd515a1bfa5d2c299da3686adfa6bee
sjtuhuoda <sjtuhuoda@sjtu.edu.cn>
2019-11-25 23:54:58 +0800

    gravity

--- app/src/main/java/com/nope/sjtu/extremecontroller/GravitySeneor.java
+++ app/src/main/java/com/nope/sjtu/extremecontroller/GravitySeneor.java
@@ -1,47 +1,76 @@
 package com.nope.sjtu.extremecontroller;
 
+import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
-//import androidx.appcompat.app.AppCompatActivity;
+import android.widget.Button;
+import android.view.View;
+import android.util.Log;
 
 public class GravitySeneor extends AppCompatActivity {
 
     private SensorManager sensorMgr;
     Sensor sensor;
-    TextView textX = null;
-    TextView textY = null;
-    TextView textZ = null;
 
     private float x, y, z;
+    private float v, r;
+    private TextView report;
+
+    private int state = 0;
+    private float vmax = 255;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if(this.getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT){
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        }
         setContentView(R.layout.activity_main);
         sensorMgr = (SensorManager) getSystemService(SENSOR_SERVICE);
         sensor = sensorMgr.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-
-        textX = (TextView) findViewById(R.id.textx);
-        textY = (TextView) findViewById(R.id.texty);
-        textZ = (TextView) findViewById(R.id.textz);
+        report=(TextView)findViewById(R.id.textView);
 
         SensorEventListener lsn = new SensorEventListener() {
             public void onSensorChanged(SensorEvent e) {
                 x = e.values[SensorManager.DATA_X];
                 y = e.values[SensorManager.DATA_Y];
                 z = e.values[SensorManager.DATA_Z];
-                setTitle("x=" + (int) x + "," + "y=" + (int) y + "," + "z="+ (int) z);
-                textX.setText("x=" + (int) x);
-                textY.setText("y=" + (int) y);
-                textZ.setText("z=" + (int) z);
+                v = state * vmax;
+                if(y>0) { r = (float) (1.0/(y+0.00001)-0.1); }
+                else { r = (float) (1.0/(y+0.00001)+0.1); }
+                report.setText(String.format("X;%f, Y:%f, Z:%f, state:%d, V:%f, R:%f",x, y, z,state,v, r));
             }
             public void onAccuracyChanged(Sensor s, int accuracy) {
             }
         };
+        Button forward = (Button) this.findViewById(R.id.forward);
+        forward.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                state = 1;
+            }
+        });
+        Button backward = (Button) this.findViewById(R.id.backward);
+        backward.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                state = -1;
+            }
+        });
+        Button stop = (Button) this.findViewById(R.id.stop);
+        stop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                state = 0;
+            }
+        });
+
         // 注册listener，第三个参数是检测的精确度
         sensorMgr.registerListener(lsn, sensor, SensorManager.SENSOR_DELAY_GAME);
     }
