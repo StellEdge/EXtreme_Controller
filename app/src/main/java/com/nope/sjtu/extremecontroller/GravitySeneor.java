5453af944630e5b748e9a4dcadbe4259d4a653b3
sjtuhuoda <sjtuhuoda@sjtu.edu.cn>
2019-11-25 21:57:13 +0800

    gravity

--- app/src/main/java/com/nope/sjtu/extremecontroller/GravitySeneor.java
+++ app/src/main/java/com/nope/sjtu/extremecontroller/GravitySeneor.java
@@ -1,4 +1,48 @@
 package com.nope.sjtu.extremecontroller;
 
-public class GravitySeneor {
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.widget.TextView;
+//import androidx.appcompat.app.AppCompatActivity;
+
+public class GravitySeneor extends AppCompatActivity {
+
+    private SensorManager sensorMgr;
+    Sensor sensor;
+    TextView textX = null;
+    TextView textY = null;
+    TextView textZ = null;
+
+    private float x, y, z;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        sensorMgr = (SensorManager) getSystemService(SENSOR_SERVICE);
+        sensor = sensorMgr.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+        textX = (TextView) findViewById(R.id.textx);
+        textY = (TextView) findViewById(R.id.texty);
+        textZ = (TextView) findViewById(R.id.textz);
+
+        SensorEventListener lsn = new SensorEventListener() {
+            public void onSensorChanged(SensorEvent e) {
+                x = e.values[SensorManager.DATA_X];
+                y = e.values[SensorManager.DATA_Y];
+                z = e.values[SensorManager.DATA_Z];
+                setTitle("x=" + (int) x + "," + "y=" + (int) y + "," + "z="+ (int) z);
+                textX.setText("x=" + (int) x);
+                textY.setText("y=" + (int) y);
+                textZ.setText("z=" + (int) z);
+            }
+            public void onAccuracyChanged(Sensor s, int accuracy) {
+            }
+        };
+        // 注册listener，第三个参数是检测的精确度
+        sensorMgr.registerListener(lsn, sensor, SensorManager.SENSOR_DELAY_GAME);
+    }
 }
